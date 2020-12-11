5fb404711c794d97c2dbb93f0001f772329150fd
IgorZasypkin <zas.i.e@yandex.ru>
2020-12-11 20:29:10 +0300

    data & requests

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,4 +5,6 @@ INSERT INTO owners (id, name) VALUES
 INSERT INTO cars
     (owner_id, name, price, city) VALUES
 (1, 'Lada', 3, 'Kazan'),
-(2, 'Volvo', 5, 'Kirov');
+(2, 'Volvo', 5, 'Kirov')
+(2, 'DAF', 10, 'Moscov')
+;
